aea6a5dbbad3043a2772d8b2a890184cd9ae751f
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-04-13 20:49:44 -0400

    basic Action

---
+++ src/Action.lua
@@ -0,0 +1,46 @@
+--[[
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+	An action is the interface to all of Locomotion's animations. All animators
+	produce an Action, which can be started, stopped, and interacted with
+	regardless of how the action moves.
+]]
+
+-- A no-op function used to avoid creating a closure for every action start.
+local function noop() end
+
+local Action = {}
+Action.__index = Action
+
+--[[
+	Creates a new action.
+]]
+function Action.new(init)
+	local self = setmetatable({
+		_init = init,
+	}, Action)
+
+	return self
+end
+
+--[[
+	Starts the action. Returns the API returned by the action init function.
+]]
+function Action:Start(callbacks)
+	callbacks = callbacks or {}
+
+	-- Get the action's API.
+	-- This may or may not be supplied by the init function.
+	local api = self._init(callbacks) or {}
+
+	-- If there's no stop action provided, use the no-op one.
+	if api.Stop == nil then
+		api.Stop = noop
+	end
+
+	return api
+end
+
+return Action
