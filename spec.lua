ec4be925ae8a5e575b51f09632b17068be504f0e
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-04-13 21:47:50 -0400

    please go away, spaces

--- spec.lua
+++ spec.lua
@@ -1,10 +1,10 @@
 --[[
 	This Source Code Form is subject to the terms of the Mozilla Public
 	License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-    This file is intended to be executed from the command line. It executes all
-    of Locomotion's test cases using lemur.
+	This file is intended to be executed from the command line. It executes all
+	of Locomotion's test cases using lemur.
 ]]
 
 local lemur = require("lib.lemur")
@@ -13,32 +13,32 @@ local habitat = lemur.Habitat.new()
 local ReplicatedStorage = habitat.game:GetService("ReplicatedStorage")
 
 local function loadFolder(path, name)
-    local folder = lemur.Instance.new("Folder")
-    folder.Name = name
-    folder.Parent = ReplicatedStorage
+	local folder = lemur.Instance.new("Folder")
+	folder.Name = name
+	folder.Parent = ReplicatedStorage
 
-    habitat:loadFromFs(path, folder)
+	habitat:loadFromFs(path, folder)
 end
 
 local function mergeUpper(root)
-    local source = root:FindFirstChild("init")
+	local source = root:FindFirstChild("init")
 
-    if source then
-        for _, child in ipairs(root:GetChildren()) do
-            child.Parent = source
-        end
+	if source then
+		for _, child in ipairs(root:GetChildren()) do
+			child.Parent = source
+		end
 
-        source.Name = root.Name
-        source.Parent = root.Parent
+		source.Name = root.Name
+		source.Parent = root.Parent
 
-        root:Destroy()
-    end
+		root:Destroy()
+	end
 
-    for _, child in ipairs(root:GetChildren()) do
-        if child:IsA("Folder") then
-            mergeUpper(child)
-        end
-    end
+	for _, child in ipairs(root:GetChildren()) do
+		if child:IsA("Folder") then
+			mergeUpper(child)
+		end
+	end
 end
 
 -- Load TestEZ into habitat's ReplicatedStorage
--- src/Action.spec.lua
+++ src/Action.spec.lua
@@ -1,76 +1,76 @@
 return function()
-    local Action = require(script.Parent.Action)
+	local Action = require(script.Parent.Action)
 
-    describe("new", function()
-        it("should create actions", function()
-            expect(Action.new(print)).to.be.ok()
-        end)
+	describe("new", function()
+		it("should create actions", function()
+			expect(Action.new(print)).to.be.ok()
+		end)
 
-        it("should error if given a bad initializer", function()
-            expect(function()
-                Action.new("hi")
-            end).to.throw()
-        end)
-    end)
+		it("should error if given a bad initializer", function()
+			expect(function()
+				Action.new("hi")
+			end).to.throw()
+		end)
+	end)
 
-    describe("Start", function()
-        it("should always return a table with a Stop function", function()
-            local dummyAction = Action.new(function() end)
-            local startResult = dummyAction:start()
+	describe("Start", function()
+		it("should always return a table with a Stop function", function()
+			local dummyAction = Action.new(function() end)
+			local startResult = dummyAction:start()
 
-            expect(typeof(startResult)).to.equal("table")
-            expect(typeof(startResult.stop)).to.equal("function")
-        end)
+			expect(typeof(startResult)).to.equal("table")
+			expect(typeof(startResult.stop)).to.equal("function")
+		end)
 
-        it("should call the initializer", function()
-            local callCount = 0
-            local action = Action.new(function()
-                callCount = callCount + 1
-            end)
+		it("should call the initializer", function()
+			local callCount = 0
+			local action = Action.new(function()
+				callCount = callCount + 1
+			end)
 
-            action:start()
-            expect(callCount).to.equal(1)
-        end)
+			action:start()
+			expect(callCount).to.equal(1)
+		end)
 
-        it("should return the API from the initializer function", function()
-            -- Declared here so we can verify that the Stop function isn't
-            -- being clobbered
-            local function stop() end
+		it("should return the API from the initializer function", function()
+			-- Declared here so we can verify that the Stop function isn't
+			-- being clobbered
+			local function stop() end
 
-            local action = Action.new(function()
-                return {
-                    test = 1,
-                    stop = stop,
-                }
-            end)
+			local action = Action.new(function()
+				return {
+					test = 1,
+					stop = stop,
+				}
+			end)
 
-            local api = action:start()
-            expect(api.test).to.equal(1)
-            expect(api.stop).to.equal(stop)
-        end)
+			local api = action:start()
+			expect(api.test).to.equal(1)
+			expect(api.stop).to.equal(stop)
+		end)
 
-        it("should pass the callbacks map to the initializer", function()
-            local function update()
-            end
+		it("should pass the callbacks map to the initializer", function()
+			local function update()
+			end
 
-            local action = Action.new(function(callbacks)
-                expect(callbacks.update).to.equal(update)
-            end)
+			local action = Action.new(function(callbacks)
+				expect(callbacks.update).to.equal(update)
+			end)
 
-            action:start({
-                update = update
-            })
-        end)
+			action:start({
+				update = update
+			})
+		end)
 
-        it("should use a single function as the update callback", function()
-            local function update()
-            end
+		it("should use a single function as the update callback", function()
+			local function update()
+			end
 
-            local action = Action.new(function(callbacks)
-                expect(callbacks.update).to.equal(update)
-            end)
+			local action = Action.new(function(callbacks)
+				expect(callbacks.update).to.equal(update)
+			end)
 
-            action:start(update)
-        end)
-    end)
+			action:start(update)
+		end)
+	end)
 end
